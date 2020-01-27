08f596bb73672f3862c21544f5519a1ab81668e8
近藤良介 <kondouryousuke@kondouryousukenoMacBook-Pro.local>
2020-01-27 20:14:21 +0900

    Use Blueprint

--- stack_knowledge/__init__.py
+++ stack_knowledge/__init__.py
@@ -3,4 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_pyfile('development.cfg')
 
+from stack_knowledge.views.entries import entry
+
+app.register_blueprint(entry, url_prefix = '/')
+
 from stack_knowledge.views import entries
--- stack_knowledge/views/entries.py
+++ stack_knowledge/views/entries.py
@@ -2,6 +2,8 @@ from flask import url_for, render_template, flash
 from flask import Blueprint
 from stack_knowledge import app
 
-@app.route('/')
+entry = Blueprint('entry', __name__)
+
+@entry.route('/')
 def show_stacks():
 	return render_template('entries/index.html')
