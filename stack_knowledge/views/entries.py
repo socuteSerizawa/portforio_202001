7e977a83018615eeb591ca38dbd37ce075b225ce
近藤良介 <kondouryousuke@kondouryousukenoMacBook-Pro.local>
2020-01-29 15:54:08 +0900

    Struct menu bar

--- stack_knowledge/views/entries.py
+++ stack_knowledge/views/entries.py
@@ -8,13 +8,25 @@ entry = Blueprint('entry', __name__)
 @entry.route('/')
 def show_stacks():
 	hoge_entry_datas = Entry.query.order_by(Entry.id.desc()).all()
-	return render_template('entry/index.html', hoge_entry_datas = hoge_entry_datas)
+	return render_template('display/index.html', hoge_entry_datas = hoge_entry_datas)
 
-@entry.route('/entry/outcomes', methods = ['GET'])
+@entry.route('/display/outcomes', methods = ['GET'])
 def new_entry():
 	return render_template('entry/outcomes.html')
 
-@entry.route('/entry/outcomes', methods = ['POST'])
+@entry.route('/display/outcomes', methods = ['GET'])
+def display_outcomes():
+	return render_template('display/outcomes.html')
+
+@entry.route('/display/authors', methods = ['GET'])
+def display_authors():
+	return render_template('display/authors.html')
+
+@entry.route('/display/subjects', methods = ['GET'])
+def display_subjects():
+	return render_template('display/subjects.html')
+
+@entry.route('/display/outcomes', methods = ['POST'])
 def entry_outcomes():
 
 	hoge_entry = Entry(
@@ -26,4 +38,4 @@ def entry_outcomes():
 	db.session.close()
 
 	hoge_entry_datas = Entry.query.order_by(Entry.id.desc()).all()
-	return render_template('entry/index.html', hoge_entry_datas = hoge_entry_datas)
+	return render_template('display/index.html', hoge_entry_datas = hoge_entry_datas)
