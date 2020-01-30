9a54990e7f8b98a6196919daa2c1e08a24af7364
近藤良介 <kondouryousuke@kondouryousukenoMacBook-Pro.local>
2020-01-30 14:49:38 +0900

    Create database and orm

--- stack_knowledge/models/entries.py
+++ stack_knowledge/models/entries.py
@@ -1,17 +1,76 @@
 from stack_knowledge import db
 from datetime import datetime
 
-class Entry(db.Model):
-	__tablename__ = 'entries'
+class Outcomes(db.Model):
+	__tablename__ = 'outcomes'
 	id = db.Column(db.Integer, primary_key = True)
-	title = db.Column(db.String(50), unique = True)
-	text = db.Column(db.Text)
 	created_at = db.Column(db.DateTime)
+	overwrite_at = db.Column(db.DateTime)
+	times = db.Column(db.Float)
+	author_id = db.Column(db.Integer, db.ForeignKey('authors.id'))
+	text = db.Column(db.String(50))
 
-	def __init__(self, title = None, text = None):
-		self.title = title
-		self.text = text
+	outcomes_related = db.relationship('OutcomesRelated', backref = 'outcome', lazy = True)
+
+	def __init__(self, overwrite_at = None, times = None, author_id = None, text = None):
 		self.created_at = datetime.utcnow()
+		self.overwrite_at = overwrite_at
+		self.times = times
+		self.author_id = author_id
+		self.text = text
 
 	def __repr__(self):
-		return '<Entry id:{} title:{} text:{}>'.format(self.id, self.title, self.text)
+		return '<Entry id:{}>'.format(self.id)
+
+class OutcomesRelated(db.Model):
+	__tablename__ = 'outcomes_related'
+	id = db.Column(db.Integer, primary_key = True)
+	outcomes_id = db.Column(db.Integer, db.ForeignKey('outcomes.id'))
+	subject_related_id = db.Column(db.Integer, db.ForeignKey('subjects.id'))
+
+	def __init__(self, outcomes_id = None, subject_related_id = None):
+		self.outcomes_id = outcomes_id
+		self.subject_related_id = subject_related_id
+
+	def __repr__(self):
+		return '<Entry id:{}>'.format(self.id)
+
+class Author(db.Model):
+	__tablename__ = 'authors'
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(50))
+
+	outcomes = db.relationship('Outcomes', backref = 'author', lazy = True)
+
+	def __init__(self, name = None):
+		self.name = name
+
+	def __repr__(self):
+		return '<Entry id:{} name:{}>'.format(self.id, self.name)
+
+class Subjects(db.Model):
+	__tablename__ = 'subjects'
+	id = db.Column(db.Integer, primary_key = True)
+	name = db.Column(db.String(50))
+
+	subjects_related = db.relationship('SubjectsRelated', backref = 'subjects', lazy = True)
+	outcomes_related = db.relationship('OutcomesRelated', backref = 'subjects', lazy = True)
+
+	def __init__(self, name = None):
+		self.name = name
+
+	def __repr__(self):
+		return '<Entry id:{} name:{}>'.format(self.id, self.name)
+
+class SubjectsRelated(db.Model):
+	__tablename__ = 'subjects_related'
+	id = db.Column(db.Integer, primary_key = True)
+	#subject_id = db.Column(db.Integer, db.ForeignKey('subjects.id'))
+	related_id = db.Column(db.Integer, db.ForeignKey('subjects.id'))
+
+	def __init__(self, subject_id = None, related_id = None):
+		#self.subject_id = subject_id
+		self.related_id = related_id
+
+	def __repr__(self):
+		return '<Entry id:{}>'.format(self.id)
--- stack_knowledge/views/entries.py
+++ stack_knowledge/views/entries.py
@@ -1,7 +1,7 @@
 from flask import url_for, render_template, flash, request
 from flask import Blueprint
 from stack_knowledge import app, db
-from stack_knowledge.models.entries import Entry
+from stack_knowledge.models.entries import *
 from stack_knowledge.models.datasets_for_html import Datasets_For_Layout
 
 entry = Blueprint('entry', __name__)
@@ -10,13 +10,36 @@ dataset_outcomes_layout = Datasets_For_Layout()
 dataset_authors_layout  = Datasets_For_Layout()
 dataset_subjects_layout = Datasets_For_Layout()
 
-dataset_outcomes_layout.set_tables('outcomes', Entry.query.order_by(Entry.id.desc()).all())
-dataset_outcomes_layout.set_tables('authors' , Entry.query.order_by(Entry.id.desc()).all())
-dataset_outcomes_layout.set_tables('subjects', Entry.query.order_by(Entry.id.desc()).all())
-
 @entry.route('/')
 def show_stacks():
-	hoge_entry_datas = Entry.query.order_by(Entry.id.desc()).all()
+
+	dataset_outcomes_layout.set_tables('outcomes', Outcomes.query.order_by(Outcomes.id.desc()).all())
+	dataset_outcomes_layout.set_tables('authors' , Outcomes.query.order_by(Outcomes.id.desc()).all())
+	dataset_outcomes_layout.set_tables('subjects', Outcomes.query.order_by(Outcomes.id.desc()).all())
+
+	hoge_entry = Outcomes(
+		overwrite_at = datetime.utcnow(),
+		times  = 1,
+		author_id = 2,
+		text = 'testd'
+		)
+	db.session.add(hoge_entry)
+	db.session.commit()
+
+	hoge_entry = Author(
+		name = 'jun2'
+		)
+	db.session.add(hoge_entry)
+	db.session.commit()
+
+	hoge_entry_datas = Outcomes.query.order_by(Outcomes.id.desc()).all()
+
+	users = db.session.query(Outcomes, Author).join(Outcomes, Outcomes.author_id == Author.id).all()
+	# users : list
+	# users[0] = (result:'Outcomes', result:'Author') : tupple
+	# users[0][0] = <class 'stack_knowledge.models.entries.Outcomes'>
+	# But 'print(users[0][0])' display __repr__()
+
 	return render_template('index.html', hoge_entry_datas = hoge_entry_datas, display_dict = dataset_outcomes_layout)
 
 @entry.route('/entry/outcomes', methods = ['GET'])
@@ -30,25 +53,16 @@ def display_outcomes():
 @entry.route('/display/authors', methods = ['GET'])
 def display_authors():
 
-	dataset_authors_layout.set_tables('authors', Entry.query.order_by(Entry.id.desc()).all())
+	dataset_authors_layout.set_tables('authors', Outcomes.query.order_by(Outcomes.id.desc()).all())
 	return render_template('display/authors.html', display_dict = dataset_authors_layout)
 
 @entry.route('/display/subjects', methods = ['GET'])
 def display_subjects():
 
-	dataset_subjects_layout.set_tables('subjects', Entry.query.order_by(Entry.id.desc()).all())
+	dataset_subjects_layout.set_tables('subjects', Outcomes.query.order_by(Outcomes.id.desc()).all())
 	return render_template('display/subjects.html', display_dict = dataset_subjects_layout)
 
 @entry.route('/display/outcomes', methods = ['POST'])
 def entry_outcomes():
 
-	hoge_entry = Entry(
-		title = request.form['title'],
-		text  = request.form['text']
-		)
-	db.session.add(hoge_entry)
-	db.session.commit()
-	db.session.close()
-
-	hoge_entry_datas = Entry.query.order_by(Entry.id.desc()).all()
 	return render_template('index.html', hoge_entry_datas = hoge_entry_datas, display_dict = dataset_outcomes_layout)
